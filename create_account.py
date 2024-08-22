704a4305de8e567b30d553e094aa609525a576c9
james <james89082@students.sbcsc.k12.in.us>
2024-08-22 15:29:41 -0400

    login

--- create_account.py
+++ create_account.py
@@ -1,5 +1,6 @@
-num_of_user_lines = int
-num_of_pass_lines = int
+num_of_user_lines = 1
+num_of_pass_lines = 1
+filer = ""
 def repeat():
     rp = input("repeat? y or n")
     if rp == "y":
@@ -7,10 +8,14 @@ def repeat():
     else:
         SystemExit
 def username_adder():
-
+    global filer
     filer = open('username.txt', "r")
     filew = open('username.txt', "a")
     wd = input("enter username: ")
+    for line in filer.readlines(num_of_user_lines):
+        if line == str(wd):
+            print("sorry that username has already been taken, please try again.")
+            repeat()
     if filer.readline(num_of_user_lines) == "":
         num_of_user_lines + 1
         filew.write(str(wd))
@@ -18,6 +23,7 @@ def username_adder():
         num_of_user_lines + 1
         filew.write("\n" + str(wd))
     return num_of_user_lines
+
 def password_adder():
     filer = open('passwords.txt', "r")
     filew = open('passwords.txt', "a")
@@ -36,5 +42,6 @@ def password_adder():
     
 
 username_adder()
+password_adder()
 
 
---
+++ login.py
@@ -0,0 +1,7 @@
+import create_account as ca
+def login():
+    acc_check = input("do you have an account? Y or N: ")
+    if acc_check == "y":
+        enter_user = input("enter username: ")
+        enter_pass = input("enter password")
+        if ca.filer
