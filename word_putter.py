387fd87efe5a8657bc4d756b3e28b380a08a8f91
james <james89082@students.sbcsc.k12.in.us>
2024-08-22 11:49:42 -0400

    psw and usr

--- word_putter.py
+++ word_putter.py
@@ -1,7 +1,40 @@
-file = open('words.txt')
-wd = input("write a word: ")
-for line in file:
-    if line == "":
-        file.write(wd)
+num_of_user_lines = int
+num_of_pass_lines = int
+def repeat():
+    rp = input("repeat? y or n")
+    if rp == "y":
+        username_adder()
     else:
-        file.write("\n" + wd)
+        SystemExit
+def username_adder():
+
+    filer = open('username.txt', "r")
+    filew = open('username.txt', "a")
+    wd = input("enter username: ")
+    if filer.readline(num_of_user_lines) == "":
+        num_of_user_lines + 1
+        filew.write(str(wd))
+    else:
+        num_of_user_lines + 1
+        filew.write("\n" + str(wd))
+    return num_of_user_lines
+def password_adder():
+    filer = open('passwords.txt', "r")
+    filew = open('passwords.txt', "a")
+    wd = input("enter password: ")
+    if filer.readline(num_of_user_lines) == "":
+        num_of_user_lines + 1
+        filew.write(str(wd))
+    else:
+        num_of_user_lines + 1
+        filew.write("\n" + str(wd))
+    return num_of_user_lines
+
+
+
+
+    
+
+username_adder()
+
+
